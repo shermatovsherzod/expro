777152fba7a23eaf8e45768c45f4706f9a1a66d9
farkhodyuldoshev <farkhodyuldoshev@users.noreply.github.com>
2020-10-29 08:05:07 +0500

    SimpleUser profile added

--- Expro/Controllers/ProfileController.cs
+++ Expro/Controllers/ProfileController.cs
@@ -40,13 +40,9 @@ namespace Expro.Controllers
             _countryService = countryService;
             _educationService = educationService;
             _workExperienceService = workExperienceService;
-        }
-        public IActionResult Index()
-        {
-            return RedirectToAction("MainInfo");
-        }
+        }      
 
-        public IActionResult MainInfo()
+        public IActionResult Index()
         {
             var currentUserAccount = accountUtil.GetCurrentUser(User);
             var currentUser = _userManager.Users.FirstOrDefault(c => c.UserName == currentUserAccount.UserName);
@@ -62,7 +58,7 @@ namespace Expro.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> MainInfo(ExpertProfileMainInfoVM vmodel)
+        public async Task<ActionResult> Index(ExpertProfileMainInfoVM vmodel)
         {
             var currentUserAccount = accountUtil.GetCurrentUser(User);
             var user = await _userManager.FindByIdAsync(currentUserAccount.ID);// Users.FirstOrDefault(c => c.UserName == currentUserAccount.UserName);
---
+++ Expro/ViewModels/SimpleUser/ProfileSimpleUserVM.cs
@@ -0,0 +1,66 @@
+﻿using Expro.Common.Utilities;
+using Expro.Models.Enums;
+using Expro.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Expro.Models
+{
+    public class ProfileSimpleUserVM
+    {
+        public ProfileSimpleUserVM()
+        {
+
+        }
+
+        [Required]
+        [Display(Name = "Имя")]
+        [StringLength(256)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        [StringLength(256)]
+        public string LastName { get; set; }
+
+        [Display(Name = "Отчество")]
+        [StringLength(256)]
+        public string PatronymicName { get; set; }
+
+        [Display(Name = "Регион")]
+        public int? RegionID { get; set; }
+
+        [Display(Name = "Город")]
+        public int? CityID { get; set; }
+
+        [Display(Name = "Другой город")]
+        [StringLength(256)]
+        //[Remote("ValidateFrom", "VideoRequest", ErrorMessage = "Введите город", AdditionalFields = "TypeID")]
+        public string CityOther { get; set; }
+       
+        [Display(Name = "Дата рождения")]
+        [Required(ErrorMessage = "Поле Дата рождения обязательно для заполнения")]
+        public string DateOfBirth { get; set; }
+              
+        [Display(Name = "Пол")]
+        public int? GenderID { get; set; }
+
+        public ProfileSimpleUserVM(ApplicationUser model) // : base(model)
+        {
+            if (model == null)
+                return;
+
+            FirstName = model.FirstName;
+            LastName = model.LastName;
+            PatronymicName = model.PatronymicName;
+            RegionID = model.RegionID;
+            CityID = model.CityID;
+            CityOther = model.CityOther;          
+            DateOfBirth = DateTimeUtils.ConvertToString(model.DateOfBirth, "dd/MM/yyyy");
+            GenderID = model.GenderID;
+        }
+    }
+}
