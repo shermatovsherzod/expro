9e23724227a99bf95cd0c9378cbceca693535eba
farkhodyuldoshev <farkhodyuldoshev@users.noreply.github.com>
2020-10-31 08:50:24 +0500

    meeeeeerge

---
+++ Expro/Areas/User/Controllers/UserPersonalAreaController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Expro.Controllers;
+using Expro.Models.Enums;
+using Expro.Services.Interfaces;
+using Expro.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Expro.Areas.User.Controllers
+{
+
+    [Area("User")]
+    public class UserPersonalAreaController : BaseController
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
