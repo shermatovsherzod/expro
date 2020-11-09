f87f2db91746543a1c23eb6ba70dce99f9f42bcf
mirazam <cortex91@inbox.ru>
2020-11-09 22:27:30 +0500

    Areas/Admin Document views refactored

--- Expro/Areas/Admin/Controllers/Document/SampleDocumentController.cs
+++ Expro/Areas/Admin/Controllers/Document/SampleDocumentController.cs
@@ -14,12 +14,6 @@ namespace Expro.Areas.Admin.Controllers
     [Area("Admin")]
     public class SampleDocumentController : BaseDocumentController
     {
-        //private readonly ISampleDocumentService SampleDocumentService;
-        //private readonly ISampleDocumentSearchService SampleDocumentSearchService;
-        //private readonly IHangfireService HangfireService;
-        //private readonly IDocumentStatusService DocumentStatusService;
-        //private readonly IDocumentAdminActionsService DocumentAdminActionsService;
-
         public SampleDocumentController(
             ISampleDocumentService sampleDocumentService,
             ISampleDocumentSearchService sampleDocumentSearchService,
@@ -33,20 +27,12 @@ namespace Expro.Areas.Admin.Controllers
                   documentAdminActionsService,
                   hangfireService)
         {
-            //SampleDocumentService = sampleDocumentService;
-            //SampleDocumentSearchService = sampleDocumentSearchService;
-            //HangfireService = hangfireService;
-            //DocumentStatusService = documentStatusService;
-            //DocumentAdminActionsService = documentAdminActionsService;
-
             ErrorDocumentNotFound = "Образцовый документ не найден";
         }
 
         public override IActionResult Index()
         {
             return base.Index();
-            //ViewData["statuses"] = DocumentStatusService.GetAsSelectList();
-            //return View();
         }
 
         [HttpPost]
@@ -55,112 +41,23 @@ namespace Expro.Areas.Admin.Controllers
             int? statusID = null, DocumentPriceTypesEnum? priceType = null)
         {
             return base.Search(draw, start, length, statusID, priceType);
-            //int recordsTotal = 0;
-            //int recordsFiltered = 0;
-            //string error = "";
-
-            //var curUser = accountUtil.GetCurrentUser(User);
-            ////ApplicationUser user = await _userManager.GetUserAsync(User);
-
-            //IQueryable<Document> dataIQueryable = SampleDocumentSearchService.Search(
-            //    start,
-            //    length,
-
-            //    out recordsTotal,
-            //    out recordsFiltered,
-            //    out error,
-
-            //    curUser.UserType.Value,
-            //    statusID,
-            //    priceType,
-            //    null,
-            //    null,
-            //    null
-            //);
-
-            //dynamic data = dataIQueryable
-            //    .ToList()
-            //    .Select(m => new DocumentListItemForAdminVM(m))
-            //    .ToList();
-
-            //return Json(new
-            //{
-            //    draw = draw,
-            //    recordsTotal = recordsTotal,
-            //    recordsFiltered = recordsFiltered,
-            //    data = data,
-            //    error = error
-            //});
         }
 
         public override IActionResult Details(int id)
         {
             return base.Details(id);
-            //var document = SampleDocumentService.GetByID(id);
-            //if (document == null)
-            //    throw new Exception("Намунавий хужжат не найден");
-
-            //DocumentDetailsForAdminVM documentVM = new DocumentDetailsForAdminVM(document);
-
-            //return View(documentVM);
         }
 
         [HttpPost]
         public override IActionResult Approve(int id)
         {
             return base.Approve(id);
-            //try
-            //{
-            //    var document = SampleDocumentService.GetByID(id);
-            //    if (document == null)
-            //        throw new Exception("Намунавий хужжат не найден");
-
-            //    var curUser = accountUtil.GetCurrentUser(User);
-
-            //    if (!DocumentAdminActionsService.ApprovingIsAllowed(document))
-            //        throw new Exception("Статус хужжата не позволяет подтвердить его");
-
-            //    //cancel request/video
-            //    DocumentAdminActionsService.Approve(document, curUser.ID);
-
-            //    //cancel hangfire jobs
-            //    HangfireService.CancelJob(document.RejectionJobID);
-
-            //    return Ok();
-            //}
-            //catch (Exception ex)
-            //{
-            //    return CustomBadRequest(ex);
-            //}
         }
 
         [HttpPost]
         public override IActionResult Reject(int id)
         {
             return base.Reject(id);
-            //try
-            //{
-            //    var document = SampleDocumentService.GetByID(id);
-            //    if (document == null)
-            //        throw new Exception("Намунавий хужжат не найден");
-
-            //    var curUser = accountUtil.GetCurrentUser(User);
-
-            //    if (!DocumentAdminActionsService.RejectingIsAllowed(document))
-            //        throw new Exception("Статус хужжата не позволяет отменить его");
-
-            //    //cancel request/video
-            //    DocumentAdminActionsService.Reject(document, curUser.ID);
-
-            //    //cancel hangfire jobs
-            //    HangfireService.CancelJob(document.RejectionJobID);
-
-            //    return Ok();
-            //}
-            //catch (Exception ex)
-            //{
-            //    return CustomBadRequest(ex);
-            //}
         }
     }
 }
---
+++ Expro/wwwroot/js/Areas/Admin/Index.js
@@ -0,0 +1,109 @@
+﻿var documentType = "";
+var documentStatuses =
+{
+    waitingForApproval: 0,
+    approved: 0,
+    rejected: 0
+};
+var documentStatusWaitingForApproval = 0;
+var documentStatusApproved = 0;
+var documentStatusRejected = 0;
+
+function SetVariables(
+    documentTypeName,
+    documentStatusesEnum)
+{
+    documentType = documentTypeName;
+    documentStatuses = documentStatusesEnum;
+}
+
+var table;
+
+$(document).ready(function () {
+    table = $("table#list").DataTable({
+        searching: false,
+        ordering: false,
+        processing: true,
+        serverSide: true,
+        lengthChange: false,
+        scrollX: true,
+        lengthChange: false,
+        ajax:
+        {
+            url: "/Admin/" + documentType + "/Search",
+            type: "POST",
+            //contentType: "application/json",
+            datatype: "json",
+            data: function (d) {
+                d.statusID = $("#statusDD").val();
+                d.priceType = $("#priceTypeDD").val();
+            },
+            //complete: function () {
+            //    StartLoadingImagesAsync();
+            //}
+        },
+        columns: [
+            {
+                "data": 0, //"name": "statusId", "autoWidth": true,
+                "render": function (data, type, full, meta) {
+                    var html = '';
+                    html += '<div class="card" style="margin-top: 10px;">';
+                    html += '   <div class="card-body">';
+                    html += '       <h3><a href="/Admin/' + documentType + '/Details/' + full.id + '">' + full.title + '</a></h3>';
+
+                    html += InsertStatusAlert(full);
+
+                    //if (full.contentType == '@((int)DocumentContentTypesEnum.file)')
+                    //    html += '   <p>Есть вложенный файл</p>';
+                    //else
+                    //    html += '   <p>' + full.text + '</p>';
+
+                    html += '       <p>Дата изменения: ' + full.dateModified + '</p>';
+                    html += '       <p>Автор: <a href="#">' + full.author.fullName + '</a></p>';
+                    html += '       <p>';
+                    for (var i = 0; i < full.lawAreas.length; i++) {
+                        html += '       <span class="badge badge-primary">' + full.lawAreas[i].name + '</span>';
+                    }
+                    html += '       </p>';
+                    //console.log(full.title);
+                    //console.log(full.priceType);
+                    if (full.priceType == '@((int)DocumentPriceTypesEnum.Paid)')
+                        html += '   <p>Цена: <b class="text-danger">' + full.priceStr + ' сум</b></p>';
+                    html += '        <a href="/Admin/' + documentType + '/Details/' + full.id + '" class="btn btn-outline-primary" target="_blank">Открыть</a>';
+                    html += '   </div>';
+                    html += '</div>';
+                    return html;
+                }
+            }
+        ]
+    });
+
+    table
+        .on('preDraw', function () {
+            $("#tableBox .spinner").show();
+        })
+        .on('draw.dt', function () {
+            $("#tableBox .spinner").hide();
+            //console.log('Redraw took at: ' + (new Date().getTime() - startTime) + 'mS');
+        });
+
+    $("#statusDD, #priceTypeDD").change(function () {
+        table.draw();
+    });
+});
+
+function InsertStatusAlert(full) {
+    var html = "<div class='alert ";
+    //if (full.status.id == "@((int)DocumentStatusesEnum.WaitingForApproval)")
+    if (full.status.id == documentStatuses.waitingForApproval)
+        html += "alert-warning'>";
+    else if (full.status.id == documentStatuses.approved)
+        html += "alert-success'>";
+    else if (full.status.id == documentStatuses.rejected)
+        html += "alert-danger'>";
+
+    html += full.status.name;
+    html += "</div>";
+
+    return html;
+}
