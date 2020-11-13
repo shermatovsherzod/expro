45939bd3b4f354be1079f42313fd7bcde75223ba
mirazam <cortex91@inbox.ru>
2020-11-13 23:12:42 +0500

    billing: ClickController, ClickTransaction model

--- Expro/Data/Migrations/20201113180447_ClickTransactionTable2.Designer.cs
+++ Expro/Data/Migrations/20201113180447_ClickTransactionTable2.Designer.cs
@@ -4,14 +4,16 @@ using Expro.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Expro.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20201113180447_ClickTransactionTable2")]
+    partial class ClickTransactionTable2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
