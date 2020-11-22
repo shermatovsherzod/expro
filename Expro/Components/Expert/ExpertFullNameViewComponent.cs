131f45385a5a3c75efde42813478a054f3a79a3c
farkhodyuldoshev <farkhodyuldoshev@users.noreply.github.com>
2020-11-22 10:56:12 +0500

    little changes in views

---
+++ Expro/Components/Expert/ExpertFullNameViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Expro.Models;
+using Expro.Utils;
+using Expro.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expro.Components
+{
+    public class ExpertFullNameViewComponent : ViewComponent
+    {
+        UserManager<ApplicationUser> _userManager;
+        public ExpertFullNameViewComponent(UserManager<ApplicationUser> userManager)
+        {
+            _userManager = userManager;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            AccountUtil accountUtil = new AccountUtil();
+            var currentUserAccount = accountUtil.GetCurrentUser(this.HttpContext.User);
+            var currentUser = _userManager.Users.FirstOrDefault(c => c.UserName == currentUserAccount.UserName);
+            ExpertFullNameVM vmodel = new ExpertFullNameVM(currentUser);
+            return View("ExpertFullName", vmodel);
+        }
+    }
+}
---
+++ Expro/ViewModels/Expert/ExpertFullNameVM.cs
@@ -0,0 +1,41 @@
+﻿using Expro.Common.Utilities;
+using Expro.Models;
+using Expro.Models.Enums;
+using Expro.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+
+namespace Expro.ViewModels
+{
+    public class ExpertFullNameVM
+    {
+        public ExpertFullNameVM()
+        {
+
+        }
+        public string Id { get; set; }
+
+        [Required]
+        [Display(Name = "Имя")]
+        [StringLength(256)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Фамилия")]
+        [StringLength(256)]
+        public string LastName { get; set; }
+
+
+        public ExpertFullNameVM(ApplicationUser model)
+        {
+            if (model == null)
+                return;
+            Id = model.Id;
+            FirstName = model.FirstName;
+            LastName = model.LastName;
+        }
+    }
+}
