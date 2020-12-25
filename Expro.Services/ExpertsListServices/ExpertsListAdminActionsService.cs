c3fda0926d7b172705a7a4825cf81135abd57121
fyuldoshev <fyuldoshev@wiut.uz>
2020-12-25 17:47:30 +0500

    Expert list part

---
+++ Expro.Services/ExpertsListServices/ExpertsListAdminActionsService.cs
@@ -0,0 +1,40 @@
+﻿using Expro.Data.Infrastructure;
+using Expro.Data.Repository.Interfaces;
+using Expro.Models;
+using Expro.Models.Enums;
+using Expro.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+namespace Expro.Services
+{
+    //public class ExpertsListAdminActionsService : IExpertsListAdminActionsService
+    //{
+    //    protected IUserService _userService;
+
+    //    public ExpertsListAdminActionsService() { }
+
+    //    public ExpertsListAdminActionsService(IUserService userService)
+    //    {
+    //        _userService = userService;
+    //    }
+
+    //    public void Approve(ApplicationUser entity, string userID)
+    //    {
+    //        entity.VacancyStatusID = (int)ExpertApproveStatusEnum.Approved;
+    //        entity.DateApproved = DateTime.Now;
+
+    //        _userService.Update(entity, userID);
+    //    }
+
+    //    public void Reject(ApplicationUser entity, string userID)
+    //    {
+    //        entity.VacancyStatusID = (int)ExpertApproveStatusEnum.Rejected;
+    //        entity.DateRejected = DateTime.Now;
+
+    //        _userService.Update(entity, userID);
+    //    }
+    //}
+}
