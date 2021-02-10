53e493b9572b6536c86327e4dfa02f2be8d59a01
fyuldoshev <fyuldoshev@wiut.uz>
2021-02-10 16:45:27 +0500

    feedback stars correction and "feedback enter" replaced to ExpertFeedsView page

---
+++ Expro/Components/Expert/ExpertRatingStarsOnlyViewComponent.cs
@@ -0,0 +1,31 @@
+﻿using Expro.Models;
+using Expro.Services.Interfaces;
+using Expro.Utils;
+using Expro.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expro.Components
+{
+    public class ExpertRatingStarsOnlyViewComponent : ViewComponent
+    {
+        IFeedbackService _feedbackService;      
+        public ExpertRatingStarsOnlyViewComponent(IFeedbackService feedbackService)
+        {            
+            _feedbackService = feedbackService;
+        }
+
+        public IViewComponentResult Invoke(string id)
+        {           
+            double overallRating = _feedbackService.GetOverallRatingByExpert(id);
+            ExpertRatingStarsTypeVM vmodel = ExpertRatingStarsTypeVM.GetExpertRatingStarsTypeVM(overallRating);
+            return View("ExpertRatingStarsOnly", vmodel);
+        }
+
+       
+    }
+}
